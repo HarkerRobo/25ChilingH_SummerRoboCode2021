b2827d640b38be4ef2b07ee61c21dcad5dac7bbf
Chiling Han <chilinghan@Chilings-MacBook-Pro.local>
2021-08-11 13:08:45 -0700

    Created Operator Interface

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,7 +7,7 @@ import harkerrobolib.wrappers.HSTalon;
 
 public class Drivetrain extends SubsystemBase
 {
-    public static Drivetrain instance;
+    private static Drivetrain instance;
     private HSTalon leftMasterMotor;
     private HSTalon rightMasterMotor;
     private HSTalon leftFollowerMotor;
@@ -16,17 +16,33 @@ public class Drivetrain extends SubsystemBase
     public static final int MOTOR_ID_LEFT_FOLLOW = 0;
     public static final int MOTOR_ID_RIGHT_MASTER = 0;
     public static final int MOTOR_ID_RIGHT_FOLLOW = 0;
+    public static final boolean IS_INVERTED_LEFT_MASTER = false;
+    public static final boolean IS_INVERTED_LEFT_FOLLOWER = false;
+    public static final boolean IS_INVERTED_RIGHT_MASTER = false;
+    public static final boolean IS_INVERTED_RIGHT_FOLLOWER = false;
     private Drivetrain()
     {
         leftMasterMotor = new HSTalon(MOTOR_ID_LEFT_MASTER);
         leftFollowerMotor = new HSTalon(MOTOR_ID_LEFT_FOLLOW);
         rightMasterMotor = new HSTalon(MOTOR_ID_RIGHT_MASTER);
         rightFollowerMotor = new HSTalon(MOTOR_ID_RIGHT_FOLLOW);
+
+    }
+    public void talonInit()
+    {
+        rightFollowerMotor.follow(rightMasterMotor);
+        leftFollowerMotor.follow(leftMasterMotor);
+        rightMasterMotor.setInverted(IS_INVERTED_RIGHT_MASTER);
+        rightFollowerMotor.setInverted(IS_INVERTED_RIGHT_FOLLOWER);
+        leftMasterMotor.setInverted(IS_INVERTED_LEFT_MASTER);
+        leftFollowerMotor.setInverted(IS_INVERTED_LEFT_FOLLOWER);
     }
     public void setArcadeDrive(double turn, double drive)
     {
+
         leftMasterMotor.set(ControlMode.PercentOutput, drive+turn);
         rightMasterMotor.set(ControlMode.PercentOutput, drive-turn);
+        
     }
     public static Drivetrain getInstance()
     {
